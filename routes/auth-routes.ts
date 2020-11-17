fa435d744fd8f3000a470f6d9a2bdf0a83949f41
RomanTokar <49822676+RomanTokar@users.noreply.github.com>
2020-11-17 13:27:44 +0200

    authRouter was added

---
+++ routes/auth-routes.ts
@@ -0,0 +1,21 @@
+import {Request, Response, Router} from 'express';
+
+const authRouter:Router = Router();
+
+authRouter.post('/register', async (req:Request, res: Response) => {
+  try{
+
+  }catch (e) {
+
+  }
+});
+
+authRouter.post('/login', async (req:Request, res: Response) => {
+  try{
+
+  }catch (e) {
+
+  }
+});
+
+export default authRouter;
