bcbe737958f8b5ee35db16ff0db5e0ad32169c56
RomanTokar <49822676+RomanTokar@users.noreply.github.com>
2020-11-17 16:01:13 +0200

    authRouter /login was added

--- routes/auth-routes.ts
+++ routes/auth-routes.ts
@@ -1,34 +1,83 @@
 import {Request, Response, Router} from 'express';
 import {User} from '../models';
 import bcrypt from 'bcrypt';
+import {check, validationResult} from 'express-validator';
 
 const authRouter = Router();
 
-authRouter.post('/register', async (req: Request, res: Response) => {
-  try {
-    const {name, email, password} = req.body;
-    const candidate = await User.findOne({email});
+authRouter.post(
+  '/register',
+  [
+    check('name', 'Maximal name length is thirty symbols.').isLength({max: 30}),
+    check('email', 'Incorrect email').isEmail(),
+    check('password')
+      .isLength({max: 30}).withMessage('Minimal password length is six symbols.')
+      .isLength({min: 8}).withMessage('Maximal password length is thirty symbols.'),
+  ],
+  async (req: Request, res: Response) => {
+    try {
+      const errors = validationResult(req);
 
-    if (candidate) {
-      res.status(400).json({message: 'This user has already existed.'});
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Incorrect data for registration.',
+        });
+      }
+
+      const {name, email, password} = req.body;
+      const candidate = await User.findOne({email});
+
+      if (candidate) {
+        res.status(400).json({message: 'This user has already existed.'});
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      await User.create({name, email, password: hashedPassword});
+
+      res.send(`User was created.`);
+    } catch (e) {
+      res.status(500).json({message: e.message});
     }
+  },
+);
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+authRouter.post(
+  '/login',
+  [
+    check('email', 'Incorrect email').isEmail(),
+    check('password', 'Password must be existed').exists(),
+  ],
+  async (req: Request, res: Response) => {
+    try {
+      const errors = validationResult(req);
 
-    await User.create({name, email, password: hashedPassword});
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Incorrect data for login',
+        });
+      }
 
-    res.send(`User was created.`);
-  } catch (e) {
-    res.status(500).json({message: e.message,})
-  }
-});
+      const {email, password} = req.body;
+      const user = await User.findOne({email});
 
-authRouter.post('/login', async (req: Request, res: Response) => {
-  try {
+      if (!user) {
+        return res.status(400).json({message: `Incorrect email or password`})
+      }
 
-  } catch (e) {
+      const isMatch = await bcrypt.compare(password, user.password);
 
-  }
-});
+      if (!isMatch) {
+        return res.status(400).json({message: 'Incorrect email or password'})
+      }
+
+      res.send(user)
+    } catch (e) {
+      res.status(500).json({message: e.message});
+    }
+  },
+);
 
 export default authRouter;
