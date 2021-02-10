d25ea685640afd8b144a90ca06ac31a1b5c6dca0
Roman Tokar <romantokar.biz@gmail.com>
2021-02-11 00:09:51 +0200

    Added responsive font sizes functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,27 @@
 import React, {useMemo} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import {Main, SignIn, SignUp} from './pages';
-import {createMuiTheme, MuiThemeProvider, PaletteType} from '@material-ui/core';
+import {
+  createMuiTheme,
+  MuiThemeProvider,
+  PaletteType,
+  responsiveFontSizes
+} from '@material-ui/core';
 import {State} from './store';
 import {useSelector} from 'react-redux';
 
 const App = () => {
   const paletteType = useSelector<State, PaletteType>(state => state.theme.paletteType);
 
-  const theme = useMemo(() => createMuiTheme({
-    palette: {
-      type: paletteType
-    }
-  }), [paletteType]);
+  const theme = useMemo(() => (
+    responsiveFontSizes(
+      createMuiTheme({
+        palette: {
+          type: paletteType
+        }
+      })
+    )
+  ), [paletteType]);
 
   return (
     <MuiThemeProvider theme={theme}>
